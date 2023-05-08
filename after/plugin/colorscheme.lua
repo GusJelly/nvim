aed5594d235220a27c4ce60020cfcbdf0bc42889
GusJelly <119959197+GusJelly@users.noreply.github.com>
2023-05-08 19:05:20 +0100

    gruvbox-transparent

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -70,9 +70,9 @@ require("gruvbox").setup({
   palette_overrides = {},
   overrides = {},
   dim_inactive = false,
-  transparent_mode = false,
+  transparent_mode = true,
 })
 
-vim.cmd("colorscheme melange")
+vim.cmd("colorscheme gruvbox")
 vim.cmd("set background=dark")
 

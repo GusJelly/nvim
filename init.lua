ea2c35ca1f15854b454bb2f96268864071f9defb
Gustavo Meireles <gustavo@unknowna8a1595012e9.lan>
2023-08-08 12:59:34 +0100

    Harpoon keybindings

--- init.lua
+++ init.lua
@@ -470,6 +470,15 @@ vim.keymap.set("n", "<leader>pv", "<cmd>:Oil<CR>")
 vim.keymap.set("n", "<leader>hg", "<cmd>:lua require('harpoon.ui').toggle_quick_menu()<CR>")
 vim.keymap.set("n", "<leader>ha", "<cmd>:lua require('harpoon.mark').add_file()<CR>")
 vim.keymap.set("n", "<leader>gl", "<cmd>:LazyGit<CR>")
+vim.keymap.set("n", "<leader>1", "<cmd>:lua require('harpoon.ui').nav_file(1)<CR>")
+vim.keymap.set("n", "<leader>2", "<cmd>:lua require('harpoon.ui').nav_file(2)<CR>")
+vim.keymap.set("n", "<leader>3", "<cmd>:lua require('harpoon.ui').nav_file(3)<CR>")
+vim.keymap.set("n", "<leader>4", "<cmd>:lua require('harpoon.ui').nav_file(4)<CR>")
+vim.keymap.set("n", "<leader>5", "<cmd>:lua require('harpoon.ui').nav_file(5)<CR>")
+vim.keymap.set("n", "<leader>6", "<cmd>:lua require('harpoon.ui').nav_file(6)<CR>")
+vim.keymap.set("n", "<leader>7", "<cmd>:lua require('harpoon.ui').nav_file(7)<CR>")
+vim.keymap.set("n", "<leader>8", "<cmd>:lua require('harpoon.ui').nav_file(8)<CR>")
+vim.keymap.set("n", "<leader>9", "<cmd>:lua require('harpoon.ui').nav_file(9)<CR>")
 -- Terminal mode keymaps
 vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>") -- Makes is so that Ctrl + \ goes into normal mode when in :term mode
 -- TABS

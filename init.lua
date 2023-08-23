aabf3edf095dab02d4483257751038e863b65057
Gustavo Meireles <gugatpm@gmail.com>
2023-08-23 21:19:32 +0100

    Lualine fixed with kickstart

--- init.lua
+++ init.lua
@@ -92,16 +92,31 @@ require('lazy').setup({
         },
     },
 
-    -- {
-    --   -- Add indentation guides even on blank lines
-    --   'lukas-reineke/indent-blankline.nvim',
-    --   -- Enable `lukas-reineke/indent-blankline.nvim`
-    --   -- See `:help indent_blankline.txt`
-    --   opts = {
-    --     char = '┊',
-    --     show_trailing_blankline_indent = false,
-    --   },
-    -- },
+    -- Lualine
+    {
+        -- Set lualine as statusline
+        'nvim-lualine/lualine.nvim',
+        -- See `:help lualine.txt`
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = 'gruvbox-material',
+                component_separators = '|',
+                section_separators = '',
+            },
+        },
+    },
+
+    {
+        -- Add indentation guides even on blank lines
+        'lukas-reineke/indent-blankline.nvim',
+        -- Enable `lukas-reineke/indent-blankline.nvim`
+        -- See `:help indent_blankline.txt`
+        opts = {
+        char = '┊',
+        show_trailing_blankline_indent = false,
+      },
+    },
 
     -- "gc" to comment visual regions/lines
     { 'numToStr/Comment.nvim', opts = {} },

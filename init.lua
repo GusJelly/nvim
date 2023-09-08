6ae3fe9b71b15ca7abd186a20475f565d831b637
Gustavo Meireles <gugatpm@gmail.com>
2023-09-08 17:51:34 +0100

    New colorscheme

--- init.lua
+++ init.lua
@@ -522,7 +522,7 @@ vim.keymap.set("n", "<leader>sc", ":!", {desc = 'Set up shell command'})
 vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", {desc = 'Zen Mode'})
 
 -- Colorscheme
-vim.cmd[[colorscheme rose-pine]]
+vim.cmd[[colorscheme vscode]]
 vim.cmd[[highlight WinSeparator guibg=none]]
 
 -- Disable cursor animations without bugs
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -336,4 +336,28 @@ return {
             vim.g.sonokai_transparent_background = 1
         end
     },
+
+    -- VScode
+    {
+        'Mofiqul/vscode.nvim',
+        lazy = true,
+        opts = {
+            -- Alternatively set style in setup
+            -- style = 'light'
+
+            -- Enable transparent background
+            transparent = true,
+
+            -- Enable italic comment
+            italic_comments = true,
+
+            -- Disable nvim-tree background color
+            disable_nvimtree_bg = true,
+
+            -- Override colors (see ./lua/vscode/colors.lua)
+            color_overrides = {
+                vscLineNumber = '#FFFFFF',
+            },
+        }
+    }
 }

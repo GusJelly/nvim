65ff1d7c169bab40ccb6fd5fe22558d9a82a3217
Gustavo Meireles <gugatpm@gmail.com>
2023-09-18 00:07:45 +0100

    More fixes this time for sure :)

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -49,8 +49,8 @@ return {
                 light = "latte",
                 dark = "mocha",
             },
-            transparent_background = true, -- disables setting the background color.
-            show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+            transparent_background = false, -- disables setting the background color.
+            show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
             term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
             dim_inactive = {
                 enabled = false, -- dims the background color of inactive window
--- lua/custom/plugins/notify.lua
+++ lua/custom/plugins/notify.lua
@@ -1,9 +1,3 @@
 return {
     'rcarriga/nvim-notify',
-    config = function()
-        vim.o.termguicolors = true
-    end,
-    opts = {
-        background_colour = "#000000",
-    }
 }

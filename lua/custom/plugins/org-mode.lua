b01280616189e986e409dc42918feef5fcd38c6d
Gustavo Meireles <gugatpm@gmail.com>
2023-10-11 17:09:21 +0100

    Orgmode now hides text markup

--- lua/custom/plugins/org-mode.lua
+++ lua/custom/plugins/org-mode.lua
@@ -21,6 +21,7 @@ return {
     require('orgmode').setup({
       org_agenda_files = '~/org/**/*',
       org_default_notes_file = '~/org/refile.org',
+      org_hide_emphasis_markers = true,
     })
   end,
 }

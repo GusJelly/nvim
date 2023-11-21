3ac349d6aa054f132626a4051e0193f20ac109e9
Gustavo Meireles <gustavo@pop.lan>
2023-11-21 00:20:56 +0000

    Reformated keymaps into separate file

--- init.lua
+++ init.lua
@@ -480,64 +480,16 @@ cmp.setup {
     },
 }
 
-
--- CUSTOM
--- vim.keymap.set("n", "<leader>pv", "<cmd>:Oil<CR>", { desc = 'enter oil.nvim' })
-vim.keymap.set("n", "<leader>pv", "<cmd>:E<CR>")
-vim.keymap.set("n", "<leader>lg", "<cmd>:LazyGit<CR>", { desc = 'spawn lazygit' })
-vim.keymap.set("n", "<leader>hg", "<cmd>:lua require('harpoon.ui').toggle_quick_menu()<CR>",
-    { desc = 'harpoon spawn gui' })
-vim.keymap.set("n", "<leader>ha", "<cmd>:lua require('harpoon.mark').add_file()<CR>", { desc = 'harpoon add file' })
-vim.keymap.set("n", "<leader>1", "<cmd>:lua require('harpoon.ui').nav_file(1)<CR>", { desc = 'harpoon go to file 1' })
-vim.keymap.set("n", "<leader>2", "<cmd>:lua require('harpoon.ui').nav_file(2)<CR>", { desc = 'harpoon go to file 2' })
-vim.keymap.set("n", "<leader>3", "<cmd>:lua require('harpoon.ui').nav_file(3)<CR>", { desc = 'harpoon go to file 3' })
-vim.keymap.set("n", "<leader>4", "<cmd>:lua require('harpoon.ui').nav_file(4)<CR>", { desc = 'harpoon go to file 4' })
-vim.keymap.set("n", "<leader>5", "<cmd>:lua require('harpoon.ui').nav_file(5)<CR>", { desc = 'harpoon go to file 5' })
-vim.keymap.set("n", "<leader>6", "<cmd>:lua require('harpoon.ui').nav_file(6)<CR>", { desc = 'harpoon go to file 6' })
-vim.keymap.set("n", "<leader>7", "<cmd>:lua require('harpoon.ui').nav_file(7)<CR>", { desc = 'harpoon go to file 7' })
-vim.keymap.set("n", "<leader>8", "<cmd>:lua require('harpoon.ui').nav_file(8)<CR>", { desc = 'harpoon go to file 8' })
-vim.keymap.set("n", "<leader>9", "<cmd>:lua require('harpoon.ui').nav_file(9)<CR>", { desc = 'harpoon go to file 9' })
-vim.keymap.set("n", "<leader>;", "<cmd>:SymbolsOutline<CR>", { desc = 'Open symbols outline' })
-
--- Terminal mode keymaps
-vim.keymap.set("t", "<C-\\>", "<C-\\><C-n>") -- Makes is so that Ctrl + \ goes into normal mode when in :term mode
-
--- TABS
-vim.keymap.set("n", "<leader>th", "<cmd>:tabprevious<CR>", { desc = 'previous tab' })
-vim.keymap.set("n", "<leader>tl", "<cmd>:tabnext<CR>", { desc = 'next tab' })
-vim.keymap.set("n", "<leader>tn", "<cmd>:tabnew<CR>", { desc = 'new tab' })
-vim.keymap.set("n", "<leader>tk", "<cmd>:tabclose<CR>", { desc = 'close current tab' })
-
--- half page jumping
-vim.keymap.set("n", "<C-d>", "<C-d>M")
-vim.keymap.set("n", "<C-u>", "<C-u>M")
-
--- BUFFERS
-vim.keymap.set("n", "[b", "<cmd>:bprevious<CR>", { desc = 'previous buffer' })
-vim.keymap.set("n", "]b", "<cmd>:bnext<CR>", { desc = 'next buffer' })
-
--- Yank to clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
--- Set up shell command
-vim.keymap.set("n", "<leader>sc", ":!", { desc = 'Set up shell command' })
-
--- ZenMode
-vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", { desc = 'Zen Mode' })
-
--- Format code in current buffer with LSP
-vim.keymap.set("n", "<leader>cf", ":Format<CR>", { desc = 'LSP: [C]ode [F]ormat' })
+require('remaps')
 
 -- Colorscheme
 vim.cmd [[colorscheme modus]]
 -- require('colorbuddy').colorscheme('gruvbuddy')
 vim.o.bg = "light"
+vim.cmd [[highlight NormalNC guibg=none]]
 
--- Highlights need to come after the background and color scheme
+-- Highlight changes needs to come after the background and color scheme
 vim.cmd [[highlight WinSeparator guibg=none]]
--- make background transparent:
--- vim.cmd[[highlight Normal guibg=none]]
 
 -- Disable cursor animations without bugs
 -- vim.opt.guicursor = "n-v-c-sm:block"
@@ -559,14 +511,14 @@ vim.keymap.set("n", "<leader>sk", "<cmd>:Telescope keymaps<CR>", { desc = 'searc
 -- vim-table-mode keybindings
 vim.keymap.set("n", "<leader>tm", "<cmd>:TableModeToggle<CR>", { desc = 'toggle table mode' })
 
-vim.opt.scrolloff = 5
+vim.opt.scrolloff = 8
 vim.opt.colorcolumn = '80'
 
 -- make indent 4 spaces
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-vim.opt.cursorline = false
+vim.opt.cursorline = true
 
 -- Move selected text:
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -586,4 +538,3 @@ vim.opt.wrap = true
 -- NetRW configuration
 -- vim.g.netrw_liststyle = 3
 vim.g.netrw_cursor = 0
-
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -91,34 +91,31 @@ return {
     -- Modus-themes from emacs
     {
         'GusJelly/modus-themes.nvim',
-        init = function()
-            require("modus-themes").setup({
-                -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
-                -- `auto` will automatically set style based on background set with vim.o.background
-                style = "auto",
-                variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = true },
-                    keywords = { italic = true },
-                    functions = {},
-                    variables = {},
-                },
+        opts = {
+            -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+            -- `auto` will automatically set style based on background set with vim.o.background
+            style = "auto",
+            variant = "default", -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+            styles = {
+                -- Style to be applied to different syntax groups
+                -- Value is any valid attr-list value for `:help nvim_set_hl`
+                comments = { italic = true },
+                keywords = { italic = true },
+                functions = {},
+                variables = {},
+            },
 
-                --- You can override specific color groups to use other groups or a hex color
-                --- function will be called with a ColorScheme table
-                ---@param colors ColorScheme
-                on_colors = function(colors) end,
+            --- You can override specific color groups to use other groups or a hex color
+            --- function will be called with a ColorScheme table
+            ---@param colors ColorScheme
+            on_colors = function(colors) end,
 
-                --- You can override specific highlights to use other groups or a hex color
-                --- function will be called with a Highlights and ColorScheme table
-                ---@param highlights Highlights
-                ---@param colors ColorScheme
-                on_highlights = function(highlights, colors)
-                end,
-            })
-        end
+            --- You can override specific highlights to use other groups or a hex color
+            --- function will be called with a Highlights and ColorScheme table
+            ---@param highlights Highlights
+            ---@param colors ColorScheme
+            on_highlights = function(highlights, colors) end,
+        },
     },
 
     -- Rose-Pine

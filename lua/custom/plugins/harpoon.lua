ac1c6d8545070a6a5823fd6a51cd5e98575ec549
Gustavo Meireles <gugatpm@gmail.com>
2023-12-14 23:13:15 +0000

    New harpoon2 branch started being used

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -5,14 +5,15 @@ return {
     config = function()
         local harpoon = require("harpoon")
 
+        ---@diagnostic disable-next-line: missing-parameter
         harpoon:setup()
 
         vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
+        vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
     end
 }

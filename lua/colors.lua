35b47b246d0757d3d0f7d1d192ce04b7a3230a70
Gustavo Meireles <gugatpm@gmail.com>
2023-12-17 00:45:07 +0000

    fuck me

--- lua/colors.lua
+++ lua/colors.lua
@@ -18,8 +18,8 @@ function ColorMyPencils()
         vim.api.nvim_set_hl(0, i, { bg = 'none' })
     end
 
-    -- vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#2f2f2f" })
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#2f2f2f" })
     -- vim.api.nvim_set_hl(0, "NotifyBackground", { bg = "#0f0f0f" })
 end
 
--- ColorMyPencils()
+ColorMyPencils()
--- lua/editor_look.lua
+++ lua/editor_look.lua
@@ -1,5 +1,5 @@
 -- Colorscheme
-vim.cmd [[colorscheme kanagawa]]
+vim.cmd [[colorscheme rose-pine]]
 -- require('colorbuddy').colorscheme('gruvbuddy')
 vim.o.bg = "dark"
 

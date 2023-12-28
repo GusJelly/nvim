ed3b392835d3eb90049a65be17c1d42510ee9f17
Gustavo Meireles <gugatpm@gmail.com>
2023-12-28 21:15:17 +0000

    Changed a bunch of stuff

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -240,9 +240,9 @@ return {
             overrides = function(colors) -- add/modify highlights
                 return {}
             end,
-            theme = "dragon", -- Load "wave" theme when 'background' option is not set
+            theme = "wave", -- Load "wave" theme when 'background' option is not set
             background = { -- map the value of 'background' option to a theme
-                dark = "dragon", -- try "dragon" !
+                dark = "wave", -- try "dragon" !
                 light = "lotus"
             },
         }

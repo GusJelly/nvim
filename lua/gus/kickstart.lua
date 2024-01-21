67b0cc02f03841f1f6976fde8f526dcd42087367
GusJelly <gugatpm@gmail.com>
2024-01-21 20:23:21 +0000

    new stuff?

--- lua/gus/kickstart.lua
+++ lua/gus/kickstart.lua
@@ -245,6 +245,7 @@ require('nvim-treesitter.configs').setup {
 
     highlight = {
         enable = true,
+        additional_vim_regex_highlighting = true,
     },
     indent = { enable = true },
     incremental_selection = {

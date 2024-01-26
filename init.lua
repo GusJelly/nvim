4d47a035eb6964bc1ab75b2516561a2a2bda161f
GusFoobar <gugatpm@gmail.com>
2024-01-26 01:15:09 +0000

    Fix: vimwiki configuration

--- init.lua
+++ init.lua
@@ -48,14 +48,15 @@ require("lazy").setup({
         {
             "vimwiki/vimwiki",
             init = function()
-            vim.cmd([[
+                -- Vimwiki configuration
+                vim.cmd([[
                 set nocompatible
                 filetype plugin on
                 syntax on
 
                 let g:vimwiki_list = [{'path': '~/vimwiki/',
-                                      \ 'syntax': 'markdown', 'ext': 'md'}]
-        ]])
+                                        \ 'syntax': 'markdown', 'ext': 'md'}]
+                ]])
             end
         },
     },

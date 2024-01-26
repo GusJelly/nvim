1aee0cccefb49bc0b087ca4d17f71da16caf0b0f
GusFoobar <gugatpm@gmail.com>
2024-01-26 04:07:09 +0000

    Added harpoon2

--- init.lua
+++ init.lua
@@ -54,6 +54,19 @@ require("lazy").setup({
             "ThePrimeagen/harpoon",
             dependencies = { "nvim-lua/plenary.nvim" },
             branch = "harpoon2",
+            config = function()
+                local harpoon = require("harpoon")
+
+                harpoon:setup()
+
+                vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+                vim.keymap.set("n", "<leader>hg", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+                vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+                vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+                vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+                vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+            end
         },
         -- Vimwiki
         {

441bce7e988b190778c5c6f77a94c3887e9666f0
GusFoobar <gugatpm@gmail.com>
2024-01-26 06:37:51 +0000

    Treesitter configuration

--- init.lua
+++ init.lua
@@ -124,6 +124,23 @@ require("lazy").setup({
         "L3MON4D3/LuaSnip",
         "saadparwaiz1/cmp_luasnip",
     },
+
+    -- Vimtex
+    {
+        'lervag/vimtex',
+        config = function()
+            vim.cmd([[
+                " This is necessary for VimTeX to load properly. The "indent" is optional.
+                " Note that most plugin managers will do this automatically.
+                filetype plugin indent on
+
+                " This enables Vim's and neovim's syntax-related features. Without this, some
+                " VimTeX features will not work (see ":help vimtex-requirements" for more
+                " info).
+                syntax enable
+            ]])
+        end
+    },
 })
 
 
--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,30 +1,49 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "query", "javascript", "awk", "rust" },
+require 'nvim-treesitter.configs'.setup {
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    ensure_installed = { "c", "lua", "vim", "query", "javascript", "awk", "rust" },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    -- List of parsers to ignore installing (or "all")
+    ignore_install = { "javascript" },
 
-  highlight = {
-    enable = true,
+    modules = {
+        ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+        -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+        ---@diagnostic disable-next-line: missing-fields
+        highlight = {
+            enable = true,
 
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "c", "vimdoc", "markdown", "markdown_inline" },
+            -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+            -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+            -- the name of the parser)
+            -- list of language that will be disabled
+            disable = {
+                "c",
+                "vimdoc",
+                "markdown",
+                "markdown_inline"
+            },
+            additional_vim_regex_highlighting = false,
+        },
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        ---@diagnostic disable-next-line: missing-fields
+        incremental_selection = {
+            enable = true,
+        },
+        ---@diagnostic disable-next-line: missing-fields
+        indent = {
+            enable = true
+        },
+
+        ---@diagnostic disable-next-line: missing-fields
+        textobjects = {
+            enable = true
+        },
+    }
 }
